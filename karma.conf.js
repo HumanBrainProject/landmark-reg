125e0f6f3ba0ad87ceaeafecb1b0c8b7ec1e0377
Yann Leprince <y.leprince@fz-juelich.de>
2017-06-28 14:39:00 +0200

    update Karma configuration to match scripts in index.html

--- karma.conf.js
+++ karma.conf.js
@@ -2,10 +2,12 @@
 module.exports = function(config) {
   config.set({
     files: [
+      "frontend/bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js",
+      "frontend/bower_components/jquery/dist/jquery.slim.js",
       "frontend/bower_components/angular/angular.js",
       "frontend/bower_components/angular-resource/angular-resource.js",
       "frontend/bower_components/angular-mocks/angular-mocks.js",
-      "frontend/bower_components/angular-bootstrap/ui-bootstrap.js",
+      //"frontend/bower_components/angular-bootstrap/ui-bootstrap.js", // unused at the moment
       "src/*.module.js",
       "src/*!(.module|.spec).js",
       "src/**/*.module.js",
