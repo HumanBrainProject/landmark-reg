3d8b892e3086aaed21be2650c558db826c6144ca
Yann Leprince <y.leprince@fz-juelich.de>
2017-06-28 12:15:43 +0200

    fix ng-disabled by switching to plain HTML (was XHTML)

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,6 +9,6 @@ app = flask.Flask(__name__, static_folder="../frontend", static_url_path="")
 
 @app.route("/")
 def root():
-    return app.send_static_file('index.xhtml')
+    return app.send_static_file('index.html')
 
 register_api(app, prefix="/api")
