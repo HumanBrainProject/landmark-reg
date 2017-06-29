b62ea688eab411333753cce6071d3985ce541e78
Yann Leprince <y.leprince@fz-juelich.de>
2017-06-29 09:41:32 +0200

    improve tests of viewer-3d-mockup

--- src/viewer/viewer-3d-mockup.component.spec.js
+++ src/viewer/viewer-3d-mockup.component.spec.js
@@ -6,15 +6,22 @@ describe("landmarkRegApp.viewer module", function() {
   describe("viewer3dMockup controller", function() {
     it("should make its own local copy of the cursor",
        inject(function($componentController) {
-         var cursorUpdateSpy = jasmine.createSpy("onCursorUpdate");
          var original_cursor = [1, 2, 3];
-         var bindings = {cursor: original_cursor, onCursorUpdate: cursorUpdateSpy};
+         var bindings = {cursor: original_cursor};
          var ctrl = $componentController("viewer3dMockup", null, bindings);
 
+         // During tests this is not called, so call it manually (maybe there
+         // is a better way?)
+         ctrl.$onChanges({cursor: {current_value: original_cursor}});
+
          expect(ctrl.cursor).toEqual(original_cursor);
-         // FIXME: the line below requires $onChanges to be called, how to
-         // trigger this?
-         //expect(ctrl.cursor).not.toBe(original_cursor);
+         expect(ctrl.cursor).not.toBe(original_cursor);
+       }));
+    it("should notify updates to the cursor",
+       inject(function($componentController) {
+         var cursorUpdateSpy = jasmine.createSpy("onCursorUpdate");
+         var bindings = {onCursorUpdate: cursorUpdateSpy};
+         var ctrl = $componentController("viewer3dMockup", null, bindings);
 
          ctrl.cursor = [4, 5, 6];
          ctrl.sendCursorUpdate();
