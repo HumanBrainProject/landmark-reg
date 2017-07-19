8cb37a3c6b848cba49d9b8dc355acde2f164ca8a
Yann Leprince <y.leprince@fz-juelich.de>
2017-07-19 12:14:40 +0200

    Improve build system
    - Improved error reporting with pump
    - Support minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,53 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var ngAnnotate = require("gulp-ng-annotate");
-//var uglify = require("gulp-uglify");
+var pump = require("pump");
 var sourcemaps = require('gulp-sourcemaps');
 var templateCache = require('gulp-angular-templatecache');
+var uglify = require("gulp-uglify");
+
+var js_sources = [
+  "src/*.module.js",
+  "src/!(*.spec).js",
+  "src/!(bower_components)/**/*.module.js",
+  "src/!(bower_components)/**/!(*.spec).js"];
+var angular_templates = "src/**/*.html";
 
 gulp.task("default", ["js", "template-cache"]);
+
 gulp.task("watch", ["js", "template-cache"], function () {
-  gulp.watch("src/**/*.js", ["js"]);
-  gulp.watch("src/**/*.html", ["template-cache"]);
+  gulp.watch(js_sources, ["js"]);
+  gulp.watch(angular_templates, ["template-cache"]);
 });
 
-gulp.task("js", function () {
-  gulp.src(["src/*.module.js",
-            "src/*.js",
-            "src/!(bower_components)/**/*.module.js",
-            "src/!(bower_components)/**/!(*.spec).js"])
-    .pipe(sourcemaps.init())
-      .pipe(concat("bundle.js"))
-      .pipe(ngAnnotate({single_quotes: false}))
-      // .pipe(uglify())
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("frontend"));
+gulp.task("js", function (cb) {
+  pump([
+    gulp.src(js_sources),
+    sourcemaps.init(),
+    ngAnnotate({single_quotes: false}),
+    concat("bundle.js"),
+    sourcemaps.write("."),
+    gulp.dest("frontend")], cb);
 });
 
-gulp.task("template-cache", function () {
-  return gulp.src("src/**/*.html")
-    .pipe(templateCache("templates.js",
-                        {
-                          standalone: true,
-                          module: "landmarkRegApp.templates"
-                        }))
-    .pipe(gulp.dest("frontend"));
+gulp.task("js-min", function (cb) {
+  pump([
+    gulp.src(js_sources),
+    sourcemaps.init(),
+    ngAnnotate({single_quotes: false}),
+    concat("bundle.js"),
+    uglify(),
+    sourcemaps.write("."),
+    gulp.dest("frontend")], cb);
+});
+
+gulp.task("template-cache", function (cb) {
+  pump([
+    gulp.src(angular_templates),
+    templateCache("templates.js",
+                  {
+                    standalone: true,
+                    module: "landmarkRegApp.templates"
+                  }),
+    gulp.dest("frontend")], cb);
 });
