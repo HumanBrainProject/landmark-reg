cbc05696b0c83f0aecc37c29e92cce19c4de1841
Yann Leprince <y.leprince@fz-juelich.de>
2017-09-26 14:29:51 +0200

    Use a more appropriate colour palette

--- src/landmarks/landmark-list.component.js
+++ src/landmarks/landmark-list.component.js
@@ -37,10 +37,10 @@
     }
 
     // Colormap from ColorBrewer:
-    // http://colorbrewer2.org/#type=qualitative&scheme=Paired&n=12
+    // http://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12
     var colours = [
-      "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c",
-      "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928"];
+      "#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462",
+      "#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"];
 
     function generate_colour() {
       var colour_use_count = colours.map(function(colour) {
