32096629aa8eb35744f6d0ec74402d533cb04f74
Yann Leprince <y.leprince@fz-juelich.de>
2017-10-24 19:27:31 +0200

    enable CORS on the backend + update dependencies

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import flask
+from flask_cors import CORS
 
 from .api import register_api
 
-
 app = flask.Flask(__name__, static_folder="../frontend", static_url_path="")
+CORS(app)
 
 @app.route("/")
 def root():
